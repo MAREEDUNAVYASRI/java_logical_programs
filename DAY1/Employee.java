a9f292a1a25b74f60358e0d3d1f251c76be17b02
MAREEDUNAVYASRI <navyamareedu325@gmail.com>
2024-11-07 21:49:03 +0530

    Add files via upload

---
+++ DAY1/Employee.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Employee
+{
+    public static void main(String[] args) 
+{
+        int Enum=10;
+        String Ename="Navya";
+        float Esal=55.67f;
+        System.out.println("the employee number is:"+Enum);
+        System.out.println("the employee name is:"+Ename);
+        System.out.println("the employee salary is:"+Esal);
+    }
+}
