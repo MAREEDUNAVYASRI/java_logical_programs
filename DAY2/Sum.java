a3d59c5e778661c92227cdb8bc4a0fd76a941516
MAREEDUNAVYASRI <navyamareedu325@gmail.com>
2024-11-10 22:56:14 +0530

    Add files via upload

---
+++ DAY2/Sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Sum
+{
+public static void main(String args[])
+{
+       Scanner sc=new Scanner(System.in);
+	int i=1, sum=0;
+System.out.println("enter a natural number:");
+	int n=sc.nextInt();
+	while(i<=1)
+	{
+	sum= sum+i;
+	i++;
+	}
+	System.out.println("sum of natural is:");
+     }
+}
