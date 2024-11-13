439ce23d2ba0c7938c84c85a73feb1f328f87374
MAREEDUNAVYASRI <navyamareedu325@gmail.com>
2024-11-13 22:40:48 +0530

    Add files via upload

---
+++ SRK A/DAY4/MM.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class MM 
+{
+public int max(int [] array)
+{
+int max = 0;
+for(int i=0; i<array.length; i++ )
+{
+if(array[i]>max) 
+{
+max = array[i];
+}
+}
+return max;
+}
+public int min(int [] array) 
+{
+int min = array[0];
+for(int i=0; i<array.length; i++ )
+{
+if(array[i]<min)
+{
+min = array[i];
+}
+}
+return min;
+}
+public static void main(String args[]) 
+{
+int[] myArray = {23, 92, 56, 39, 93};
+MM m = new MM();
+System.out.println("Maximum value in the array is::"+m.max(myArray));
+System.out.println("Minimum value in the array is::"+m.min(myArray));
+}
+}
